ac4ecca77587299ed2e65b19753811a714f702d7
Asmaul Mallick <145951970+Asmaul360@users.noreply.github.com>
2026-04-25 18:15:12 +0530

    level order traversal

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class LevelOrderTraversal{
+
+    // Level Order Traversal (BFS)
+    public static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node current = q.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                q.add(current.left);
+            }
+
+            if (current.right != null) {
+                q.add(current.right);
+            }
+        }
+    }
+
+    // Insert into Binary Tree (level order insertion)
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+
+            if (temp.left == null) {
+                temp.left = new Node(data);
+                break;
+            } else {
+                q.add(temp.left);
+            }
+
+            if (temp.right == null) {
+                temp.right = new Node(data);
+                break;
+            } else {
+                q.add(temp.right);
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // number of nodes
+        Node root = null;
+
+        for (int i = 0; i < n; i++) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+
+        levelOrder(root);
+    }
+}
